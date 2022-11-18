b3ac0bb101e30e2cd9e4c199251c271e7ab05806
SRCinVA <scitarella@hotmail.com>
2022-11-17 19:50:27 -0500

    Window sizing

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from kivy.app import App
+from kivy.properties import NumericProperty
 from kivy.uix.widget import Widget
 
 class MainWidget(Widget):
-    pass
+    perspective_point_x = NumericProperty(0)
+    perspective_point_y = NumericProperty(0)
+
+    def __init__(self, **kwargs):
+        super(MainWidget, self).__init__(**kwargs)
+        print("INIT W: " + str(self.width) + " H: " + str(self.height)) # from the __init__ function, the window can report its default size
+
+    def on_parent(self, widget, parent):  # this is when we attach the widget to the app.
+        # print("ON PARENT INIT W: " + str(self.width) + " H: " + str(self.height))
+        pass
+
+    def on_size(self, *args):  # are these built-in functions?
+        print("ON SIZE INIT W: " + str(self.width) + " H: " + str(self.height))
+
 
 class GalaxyApp(App):
     pass
